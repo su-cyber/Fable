a6f1414ad9b078ad93523d4f56b641e1632af0d2
Luigi <66072698+lui-dias@users.noreply.github.com>
2022-03-27 17:53:32 -0300

    Remove ping command

--- commands/ping.ts
+++
@@ -1,7 +0,0 @@
-import { MyCommandSlashBuilder } from '../src/lib/builders/slash-command'
-
-export default new MyCommandSlashBuilder({ name: 'ping', description: 'Pong!' }).setDo(async interaction => {
-    await interaction.reply({
-        content: 'Pong!',
-    })
-})
