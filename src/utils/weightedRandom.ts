7371cacd6a2dd292dfa041815d20eb03929da40d
Luigi <66072698+lui-dias@users.noreply.github.com>
2022-03-27 20:42:30 -0300

    replace logic from python random.choices

--- src/utils/weightedRandom.ts
+++ src/utils/weightedRandom.ts
@@ -1,8 +1,30 @@
 import range from 'lodash.range'
 
+function accumulate(arr) {
+    const n = arr.length
+    const out = new Array(n)
+    out[0] = arr[0]
+    for (let i = 1; i < n; i++) {
+        out[i] = out[i - 1] + arr[i]
+    }
+    return out
+}
+
+function bisect_right(a, x, lo, hi) {
+    while (lo < hi) {
+        var mid = Math.floor((lo + hi) / 2)
+        if (a[mid] < x) {
+            lo = mid + 1
+        } else {
+            hi = mid
+        }
+    }
+    return lo
+}
+
 /**
  * Choose a random item with probability
- * 
+ *
  * You can exceed the weight to more than 100%, it will work
  *
  * ```js
@@ -13,18 +35,20 @@ import range from 'lodash.range'
  * weightedRandom(item, weights)
  * ````
  */
-export function weightedRandom(items: any[], weights: number[]) {
-    const r = range(weights.length)
+export function weightedRandom(values, weights, k = 1) {
+    const n = values.length
+    const cum = accumulate(weights)
+    const hi = n - 1
 
-    r.forEach(i => {
-        weights[i] += weights[i - 1] || 0
-    })
-
-    const random = Math.random() * weights[weights.length - 1]
-
-    for (const i of r) {
-        if (weights[i] > random) {
-            return items[i]
-        }
+    if (k === 1) {
+        const r = Math.random() * cum[hi]
+        const j = bisect_right(cum, r, 0, hi)
+        return values[j]
     }
+
+    return range(k).map(i => {
+        const r = Math.random() * cum[hi]
+        const j = bisect_right(cum, r, 0, hi)
+        return values[j]
+    })
 }
