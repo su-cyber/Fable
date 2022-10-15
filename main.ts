ddeb215b304974bc644173d98ce18d3f21cc375a
Dante8738 <sumitkundu477@gmail.com>
2022-10-15 17:51:24 +0530

    .

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-import { Intents } from 'discord.js'
+import { Client, Intents } from 'discord.js'
 import { Bot } from './src/bot'
 
 
 new Bot({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }).run()
+
 
