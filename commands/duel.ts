cf157beea44d1213c2a4b70f9818f28adf5a376b
Dante8738 <sumitkundu477@gmail.com>
2022-10-16 12:58:02 +0530

    .

--- commands/duel.ts
+++ commands/duel.ts
@@ -38,63 +38,7 @@ export default new MyCommandSlashBuilder({ name: 'duel', description: 'Duel with
                             if(result){
                                 const attacker = Warrior.create(author)
                                 const defender = Warrior.create(opponent)
-                                profileModel.findOne({userID:authorId},async function(err,foundUser) {
-                                    if(err){
-                                        console.log(err);
-                                        
-                                    }
-                                    else{
-                                        console.log("called");
-                                        
-                                        
-                                        attacker.health=foundUser.health
-                                        attacker.mana=foundUser.mana
-                                        attacker.armor=foundUser.armour
-                                        attacker.magicPower=foundUser.magicPower
-                                        attacker.attackDamage=foundUser.attackDamage
-                                        attacker.evasion=foundUser.evasion
-                                        attacker.maxHealth=foundUser.health
-                                        attacker.skills=foundUser.skills
-                                        attacker.passive_skills = foundUser.passiveskills
-
-                                        if(foundUser.weapon.length === 0){
-                                            attacker.skills=foundUser.magicskills
-                                        }
-                                        else{
-                                            
-                                            attacker.skills=foundUser.weaponskills.concat(foundUser.magicskills,foundUser.weapon[0].skills)
-                                        }
-                          
-                                    }
-                                })
-                                profileModel.findOne({userID:opponentId},async function(err,foundOpp) {
-                                    if(err){
-                                        console.log(err);
-                                        
-                                    }
-                                    else{
-                                        console.log("called");
-                                        
-                                        defender.health=foundOpp.health
-                                        defender.mana=foundOpp.mana
-                                        defender.armor=foundOpp.armour
-                                        defender.magicPower=foundOpp.magicPower
-                                        defender.attackDamage=foundOpp.attackDamage
-                                        defender.evasion=foundOpp.evasion
-                                        defender.maxHealth=foundOpp.health
-                                        defender.skills=foundOpp.skills
-                                        defender.passive_skills = foundOpp.passiveskills
-
-                                        if(foundOpp.weapon.length === 0){
-                                            defender.skills=foundOpp.magicskills
-                                        }
-                                        else{
-                                            
-                                            defender.skills=foundOpp.weaponskills.concat(foundOpp.magicskills,foundOpp.weapon[0].skills)
-                                        }
-                                        
-                                    }
-                                })
+                                
                                 await new PvPDuel({
                                     interaction,
                                     player1: attacker,
@@ -163,8 +107,66 @@ class PvPDuel extends DuelBuilder {
     }
 
     async beforeDuelStart() {
-        
-        
+        const authorId = this.interaction.user.id
+        const opponentId = this.interaction.options.getUser('user').id
+
+        profileModel.findOne({userID:authorId},async function(err,foundUser) {
+            if(err){
+                console.log(err);
+                
+            }
+            else{
+                console.log("called");
+                
+                
+                this.player1.health=foundUser.health
+                this.player1.mana=foundUser.mana
+                this.player1.armor=foundUser.armour
+                this.player1.magicPower=foundUser.magicPower
+                this.player1.attackDamage=foundUser.attackDamage
+                this.player1.evasion=foundUser.evasion
+                this.player1.maxHealth=foundUser.health
+                this.player1.skills=foundUser.skills
+                this.player1.passive_skills = foundUser.passiveskills
+
+                if(foundUser.weapon.length === 0){
+                    this.player1.skills=foundUser.magicskills
+                }
+                else{
+                    
+                    this.player1.skills=foundUser.weaponskills.concat(foundUser.magicskills,foundUser.weapon[0].skills)
+                }
+  
+            }
+        })
+        profileModel.findOne({userID:opponentId},async function(err,foundOpp) {
+            if(err){
+                console.log(err);
+                
+            }
+            else{
+                console.log("called");
+                
+                this.player2.health=foundOpp.health
+                this.player2.mana=foundOpp.mana
+                this.player2.armor=foundOpp.armour
+                this.player2.magicPower=foundOpp.magicPower
+                this.player2.attackDamage=foundOpp.attackDamage
+                this.player2.evasion=foundOpp.evasion
+                this.player2.maxHealth=foundOpp.health
+                this.player2.skills=foundOpp.skills
+                this.player2.passive_skills = foundOpp.passiveskills
+
+                if(foundOpp.weapon.length === 0){
+                    this.player2.skills=foundOpp.magicskills
+                }
+                else{
+                    
+                    this.player2.skills=foundOpp.weaponskills.concat(foundOpp.magicskills,foundOpp.weapon[0].skills)
+                }
+                
+            }
+        })
         if(this.player1.passive_skills.length !=0){
             let i
             for(i=0;i<this.player1.passive_skills.length;i++){
