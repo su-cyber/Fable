6854d44c6df9b628c31764a088fac2b676238480
Dante8738 <sumitkundu477@gmail.com>
2022-12-22 21:04:50 +0530

    private v4

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-import { Client, Intents } from 'discord.js'
+import { Client, Intents, PartialDMChannel } from 'discord.js'
 import { Bot } from './src/bot'
 
 
-new Bot({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] }).run()
+new Bot({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS],partials: ['CHANNEL'] }).run()
 
 
