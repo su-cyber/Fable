d5eff40434fdd87a520ca95aefe15fd90b23d0bb
Dante8738 <sumitkundu477@gmail.com>
2023-02-20 23:21:17 +0530

    img manip v7

--- commands/test_img.ts
+++ commands/test_img.ts
@@ -1,7 +1,8 @@
 import { MyCommandSlashBuilder } from '../src/lib/builders/slash-command'
 import profileModel from '../models/profileSchema'
 import { Collector, MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu, MessageComponentInteraction,CacheType, MessageAttachment} from 'discord.js'
-import Jimp from "jimp";
+import * as canvas from 'skia-canvas/lib'
+
 
 export default new MyCommandSlashBuilder({ name: 'img', description: 'testing img' })
 
@@ -10,19 +11,17 @@ export default new MyCommandSlashBuilder({ name: 'img', description: 'testing im
         const authorId = interaction.user.id
         const guildID = interaction.guildId;
 
-        Jimp.read("assets/AubeTown/Ghorgon.jpeg")
-        .then((image) => {
-    // Do stuff with the image.
-        Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
-        const clone = image.clone()
-        clone.print(font, 10, 10, "Hello world!");
-        
-        clone.write("assets/AubeTown/test.jpeg");
-            const attachment = new MessageAttachment("assets/AubeTown/test.jpeg")
+        let img = await canvas.loadImage("assets/AubeTown/Ghorgon.jpeg")
+        const src = new canvas.Canvas(400,400)
+        let ctx = src.getContext("2d")
+        ctx.drawImage(img,0,0)
+        ctx.font = "50px serif";
+        ctx.fillText("Hello world", 50, 90);
+        src.saveAs("assets/AubeTown/test.jpeg")
+        const attachment = new MessageAttachment("assets/AubeTown/test.jpeg")
         interaction.reply({files:[attachment]})
-       
         
-      });
-  })
+        
+        
         
     })
