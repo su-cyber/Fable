f2a8e8263888ed6a1d9d40424070a9b681461e83
Dante8738 <sumitkundu477@gmail.com>
2023-02-20 19:24:28 +0530

    img manip v5

--- commands/test_img.ts
+++ commands/test_img.ts
@@ -16,11 +16,11 @@ export default new MyCommandSlashBuilder({ name: 'img', description: 'testing im
         Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then((font) => {
         const clone = image.clone()
         clone.print(font, 10, 10, "Hello world!");
-        let file = "C:/Users/Sumit Kundu/Age-2/assets/AubeTown/test_img." + image.getExtension();
-        image.write(file);
-            const attachment = new MessageAttachment(file)
+        
+        image.write("assets/AubeTown/test.jpeg");
+            const attachment = new MessageAttachment("assets/AubeTown/test.jpeg")
         interaction.reply({files:[attachment]})
-        console.log(file);
+       
         
       });
   })
