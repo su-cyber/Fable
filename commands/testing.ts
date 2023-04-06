d54ce57fb7ec8df0e8a4ceb57be48c026e197594
Dante8738 <sumitkundu477@gmail.com>
2023-04-06 22:26:42 +0530

    statscreen test

--- commands/testing.ts
+++ commands/testing.ts
@@ -11,16 +11,17 @@ export default new MyCommandSlashBuilder({ name: 'img', description: 'testing im
         const authorId = interaction.user.id
         const guildID = interaction.guildId;
 
-    let img = await loadImage("assets/AubeTown/Ghorgon.jpeg")
-    registerFont('fonts/DellaRespira.ttf', { family: 'serif' })
+    let img = await loadImage("assets/AubeTown/statscreen.png")
+   
+    registerFont('fonts/DellaRespira.ttf', { family: 'DellaRespira' })
         const src = new Canvas(400,300)
         let ctx = src.getContext("2d")
         ctx.drawImage(img,0,0)
         ctx.font = '12px "serif"'
         ctx.fillText(`${interaction.user.username}`, 50, 90);
-        const buffer = await src.toBuffer('image/jpeg')
+        const buffer = await src.toBuffer('image/png')
         const attachment = await new MessageAttachment(buffer)
-        interaction.reply({content:"NICE",files:[attachment]})
+        interaction.reply({files:[attachment]})
         
 
         
