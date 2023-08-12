f25dfd7142a9ae42319a29242643626e9585a426
Dante8738 <sumitkundu477@gmail.com>
2023-08-13 00:07:31 +0530

    ranger license start

---
+++ commands/ranger_license.ts
@@ -0,0 +1,56 @@
+import { MyCommandSlashBuilder } from '../src/lib/builders/slash-command'
+import profileModel from '../models/profileSchema'
+import {MessageAttachment} from 'discord.js'
+import { loadImage,Canvas,registerFont } from 'canvas'
+import { SlashCommandUserOption } from '@discordjs/builders'
+
+
+export default new MyCommandSlashBuilder({ name: 'shop', description: 'Access the Shop' })
+.addUserOption((option: SlashCommandUserOption) =>
+option.setName('user').setDescription(`View user's license`).setRequired(false)
+)
+    .setDo(
+    async (bot, interaction) => {
+        const authorId = interaction.user.id;
+        const guildID = interaction.guildId;
+        const userId = interaction.options.getUser('user').id
+
+        profileModel.exists({userID:authorId},async function(err,res){
+            if(err){
+                console.log(err);
+                
+            }
+            else{
+                if(res){
+                    console.log(userId);
+                    
+                    profileModel.exists({userID:authorId},async function(err,res){
+                        if(err){
+                            console.log(err);
+                            
+                        }
+                        else{
+                            if(res){
+
+                                let img
+                                let path
+                                const name = interaction.user.username
+                                profileModel.findOne({userID:authorId},async function(err,foundUser){
+            
+            
+                                })
+                            }
+                            else{
+                                interaction.reply({content:`It seems that the mentioned User is not awakened yet!`,ephemeral:true},)
+                            }
+                        }
+                    })
+                   
+
+                }
+                else{
+                    interaction.reply({content:`It seems that you are not awakened yet!`,ephemeral:true},)
+                }
+            }
+        })
+    })
