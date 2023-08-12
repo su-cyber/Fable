4702e13ea69b339ff676e86ae74e3df5ac936da6
Dante8738 <sumitkundu477@gmail.com>
2023-08-13 01:51:30 +0530

    ranger license test 10

--- commands/ranger_license.ts
+++ commands/ranger_license.ts
@@ -58,7 +58,7 @@ option.setName('user').setDescription(`View user's license`).setRequired(false)
                             ctx.fillStyle = "#E29A37"
                             ctx.fillText(`${name}`, 40, 910);
                             ctx.font = '38px "serif"'
-                            ctx.fillText(`${title}`, 273, 57);
+                            ctx.fillText(`${title}`, 253, 57);
                             
 
 
@@ -82,7 +82,45 @@ option.setName('user').setDescription(`View user's license`).setRequired(false)
                                     let path
                                     const name = user.username
                                     profileModel.findOne({userID:user.id},async function(err,foundUser){
-                
+                                        if(foundUser.guild == "None"){
+                                            interaction.reply({content:`You have not recieved your Ranger License yet!`,ephemeral:true})
+                                        }
+                                        else{
+                                        path = `assets/Ranger_License/${foundUser.class}_Bronze_license.jpeg`
+                                        img = await loadImage(path)
+            
+            
+                                        const title = foundUser.current_title[0].toUpperCase()
+                                        const level = foundUser.level
+                                        const grade = foundUser.ranger_grade
+                                        const rank = foundUser.guild_rank.toUpperCase()
+                                        const guild = `${foundUser.guild.toUpperCase()} GUILD RANGER`
+                                        registerFont('fonts/DellaRespira.ttf', { family: 'DellaRespira' })
+                                        const src = new Canvas(822,1122)
+                                        let ctx = src.getContext("2d")
+                                        ctx.drawImage(img,0,0)
+            
+                                        ctx.font = '58px "serif"'
+                                        ctx.fillStyle = "#E29A37"
+                                        ctx.fillText(`${level}`, 78, 820);
+                                        ctx.font = '32px "serif"'
+                                        ctx.fillText(`${grade}`, 292, 812);
+                                        ctx.fillText(`${rank}`, 515, 814);
+                                        ctx.font = 'bold 18px "serif"'
+                                        ctx.fillStyle = "black"
+                                        ctx.fillText(`${guild}`, 485, 906);
+                                        ctx.font = '26px "serif"'
+                                        ctx.fillStyle = "#E29A37"
+                                        ctx.fillText(`${name}`, 40, 910);
+                                        ctx.font = '38px "serif"'
+                                        ctx.fillText(`${title}`, 253, 57);
+                                        
+            
+            
+                                        const buffer = await src.toBuffer('image/jpeg')
+                                        const attachment = await new MessageAttachment(buffer)
+                                        interaction.reply({files:[attachment],ephemeral:true})
+                                        }
                 
                                     })
                                 }
