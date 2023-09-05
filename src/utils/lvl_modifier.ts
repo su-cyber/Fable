73654bc6d14205e9c99e282c533f69299a8ae4ee
Dante8738 <sumitkundu477@gmail.com>
2023-09-05 10:14:32 +0530

    damage update

--- src/utils/lvl_modifier.ts
+++ src/utils/lvl_modifier.ts
@@ -1,16 +1,17 @@
 export default function lvl_modifier(lvl:number){
     let lvl_mod
     if (lvl >= 1 && lvl < 9) {
-        lvl_mod = Math.pow(4 + (lvl * 0.7), 2);
+        lvl_mod = Math.pow(4 + (lvl * 0.7), 2) / 1.4;
     } else if (lvl >= 9 && lvl < 18) {
-        lvl_mod = Math.pow(5 + (lvl * 0.53), 2);
+        lvl_mod = Math.pow(5 + (lvl * 0.55), 2) / 1.5;
     } else if (lvl >= 18 && lvl < 30) {
-        lvl_mod = Math.pow(7 + (lvl * 0.43), 2) - 20;
+        lvl_mod = (Math.pow(7 + (lvl * 0.43), 2) - 20) / 1.5;
     } else if (lvl >= 30 && lvl <= 50) {
-        lvl_mod = Math.pow(10 + (lvl * 0.325), 2) - 25;
+        lvl_mod = (Math.pow(10 + (lvl * 0.325), 2) - 25) / 1.5;
     } else {
-        lvl_mod = Math.pow(15 + (lvl * 0.23), 2) - 45;
+        lvl_mod = Math.pow(15 + (lvl * 0.215), 2) / 1.5;
     }
 
     return lvl_mod;
+
 }
