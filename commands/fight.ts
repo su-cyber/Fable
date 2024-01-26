24d64bed21a697e70f4e0432322f0b8d4fbe942b
sumit kundu <sumitkundu477@gmail.com>
2024-01-26 23:28:52 +0530

    debugging passives

--- commands/fight.ts
+++ commands/fight.ts
@@ -491,6 +491,8 @@ export class PvEDuel extends DuelBuilder {
 
                 if(this.attacker.passive_skills.length !=0){
                     let i
+                    console.log(this.attacker.passive_skills);
+                    
                     for(i=0;i<this.attacker.passive_skills.length;i++){
                         const passive_skill = passive_skills.find(skill => skill.name === this.attacker.passive_skills[i].name)
                         this.attacker.useSkill(this.attacker,this.defender,passive_skill)
--- src/age/heroes/allPassives.ts
+++ src/age/heroes/allPassives.ts
@@ -1,11 +1,12 @@
 import dragoon_tree from "../skills/dragoon_tree";
 import buushin_passive from "./buushin_passive";
+import dragoon_passive from "./dragoon_passive";
 import gladius_passive from "./gladius_passive";
 import magus_passive from "./magus_passive";
 import noir_passive from "./noir_passive";
 import other_passive_skills from "./passive_skills";
 
 
-const passive_skills = other_passive_skills.concat(noir_passive,buushin_passive,gladius_passive,magus_passive,dragoon_tree)
+const passive_skills = other_passive_skills.concat(noir_passive,buushin_passive,gladius_passive,magus_passive,dragoon_passive)
 
 export default passive_skills
